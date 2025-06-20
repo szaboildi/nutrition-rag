60a4b1a47ac325331168d5f67adaaf6190b7044a
szaboildi <szabo.ildikomse@gmail.com>
2025-06-20 11:53:36 +0200

    COSM: adjust the look of the app and cleanup

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -1,5 +1,4 @@
 import os
-# os.environ["STREAMLIT_SERVER_ENABLE_FILE_WATCHER"] = "false"  # Disables problematic inspection
 
 import streamlit as st
 from nutritionrag.rag_pipeline import rag_setup_qdrant, rag_query_once_qdrant
@@ -21,15 +20,16 @@ vector_db_client, encoder, llm_client = rag_setup_qdrant(
     config=config[config_name], from_scratch=from_scratch)
 
 
-
 # App functionality
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
-    st.session_state.placeholder = "Your query goes here"
+    st.session_state.placeholder = "Your question goes here"
+
+st.title("Nutrition Chat Bot")
 
 query = st.text_input(
-    "Please put your query below",
+    "Please put your question below",
     label_visibility=st.session_state.visibility,
     disabled=st.session_state.disabled,
     placeholder=st.session_state.placeholder)
