54cbe8a81d3744b102bd7dbd926042bfae3cb17d
szaboildi <szabo.ildikomse@gmail.com>
2025-06-20 11:53:23 +0200

    FUNC: add more printouts to the app

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -36,7 +36,24 @@ query = st.text_input(
 
 
 if query:
-    st.write("Fetching your query...")
+    st.empty()
     rag_response = rag_query_once_qdrant(
         query, vector_db_client, encoder, llm_client, config[config_name])
-    st.write(rag_response[1])
+    st.subheader(rag_response[1])
+
+    if len(rag_response[2]["retrieved"])==0:
+        st.write("We were unable to find what you're looking for")
+    else:
+        st.markdown("---")
+        st.write("We based this response on the following answers from our catalogue:")
+        st.markdown("---")
+        unique_answers = []
+        for item in rag_response[2]["retrieved"]:
+            if item["answer"] not in unique_answers:
+                unique_answers.append(item["answer"])
+                st.write(f'Question: {item["question"]}')
+                st.write(f'Answer: {item["answer"]}')
+                st.write(f'Similarity score ({config[config_name]["distance_type"].lower()}): {item[config[config_name]["distance_type"].lower()]}')
+                st.markdown("---")
+            if len(unique_answers) > 2:
+                break
